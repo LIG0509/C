77de19989f2d8602b5fd00a9a84c2eec65c47c3f
LIG0509 <81171835+LIG0509@users.noreply.github.com>
2021-03-27 19:41:42 +0800

    利用指针查找成绩并输出
    不知道怎么用指针输入

---
+++ 指针与数组1.c
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+int main()
+{
+	void search(float(*p)[4], int n);
+	float score[3][4];
+	int m, s;
+	float *p;
+	p = &score[0][0];
+	for ( m = 0; m < 3; m++)
+	{
+		for (s = 0; s < 4; s++)
+		{
+			scanf_s("%f",&score[m][s]);
+		}
+	}
+	search(score, 3);
+	return 0;
+}
+void search(float(*p)[4], int n)
+{
+	int i, j, flag;
+	for (j= 0; j < n; j++)
+	{
+		flag = 0;
+		for (i = 0; i < 4; i++)
+			if (*(*(p + j) + i) < 60) flag = 1;
+		if (flag == 1)
+		{
+			printf("No.%d fails,his scores are:\n", j + 1);
+			for (i = 0; i < 4; i++)
+			{
+				printf("%5.lf", *(*(p + j) + i));
+			}
+			printf("\n");
+		}
+	}
+}
